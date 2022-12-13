2f18f9613aa94c9786fc95a7f5e6dab6334a2276
Paloma <palososa_19@hotmail.es>
2022-12-13 00:42:58 -0300

    BaseCuenta actualizado

--- SoyViajero.BBDD/Data/BaseCuenta.cs
+++ SoyViajero.BBDD/Data/BaseCuenta.cs
@@ -27,8 +27,6 @@ namespace SoyViajero.BBDD.Data
         public string Pais { get; set; }
         public string Descripcion { get; set; }
         public string FotoPerfil { get; set; }
-
-        public MemoryStream ImagenPerfil { get; set; }
         #endregion
     }
 }
